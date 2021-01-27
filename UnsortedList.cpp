aecd41c559fb935777bcf5ff283536dccce1c767
DrErickson <77122859+DrErickson@users.noreply.github.com>
2021-01-26 20:58:54 -0800

    Converted to linked list implementation. Converted to template. Modified CMakeLists to exclude UnsortedList.cpp to fix linker errors. Fixed some errors in the TestDriverData and TestDriver.cpp functions. Made output of TestDriver.cpp slightly prettier.

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -1,1 +1,46 @@
+#include "UnsortedList.h"
 
+template<class T>
+UnsortedList<T>::UnsortedList() {
+
+}
+
+template<class T>
+void UnsortedList<T>::MakeEmpty() {
+
+}
+
+template<class T>
+bool UnsortedList<T>::IsFull() const {
+    return false;
+}
+
+template<class T>
+int UnsortedList<T>::GetLength() const {
+    return length;
+}
+
+template<class T>
+bool UnsortedList<T>::Contains(T someItem) {
+    return false;
+}
+
+template<class T>
+void UnsortedList<T>::PutItem(T item) {
+
+}
+
+template<class T>
+void UnsortedList<T>::DeleteItem(T item) {
+
+}
+
+template<class T>
+void UnsortedList<T>::ResetIterator() {
+
+}
+
+template<class T>
+int UnsortedList<T>::GetNextItem() {
+    return 0;
+}
--- UnsortedList.h
+++ UnsortedList.h
@@ -1,64 +1,71 @@
 #ifndef UNSORTEDLIST_H
 #define UNSORTEDLIST_H
 
-const int MAX_ITEMS = 5;
+template<class T>
+struct Node {
+    T value;
+    Node* next;
+};
 
+template<class T>
 class UnsortedList
 {
 public:
-  UnsortedList();
-  // Constructor
-  
-  void MakeEmpty();
-  // Function: Returns the list to the empty state.
-  // Post:  List is empty.
-  
-  bool IsFull() const;
-  // Function:  Determines whether list is full.
-  // Pre:  List has been initialized.
-  // Post: Function value = (list is full)
+    UnsortedList();
+    // Constructor
 
-  int GetLength() const;
-  // Function: Determines the number of elements in list.
-  // Pre:  List has been initialized.
-  // Post: Function value = number of elements in list
+    void MakeEmpty();
+    // Function: Returns the list to the empty state.
+    // Post:  List is empty.
 
-  bool Contains(int someItem);
-  // Function: Determines if someItem is in the list.
-  // Pre:  List has been initialized.
-  // Post: If there is an element someItem whose key matches
-  //       item's key, then the function returns true.
-  // 	     otherwise it will return false. List is unchanged.
+    bool IsFull() const;
+    // Function:  Determines whether list is full.
+    // Pre:  List has been initialized.
+    // Post: Function value = (list is full)
 
-  void PutItem(int item);
-  // Function: Adds item to list.
-  // Pre:  List has been initialized.
-  //       List is not full.
-  // Post: item is in list.
+    int GetLength() const;
+    // Function: Determines the number of elements in list.
+    // Pre:  List has been initialized.
+    // Post: Function value = number of elements in list
 
-  void DeleteItem(int item);
-  // Function: Deletes the first element whose key matches item's key.
-  // Pre:  ???
-  // Post: ???    
+    bool Contains(T someItem);
+    // Function: Determines if someItem is in the list.
+    // Pre:  List has been initialized.
+    // Post: If there is an element someItem whose key matches
+    //       item's key, then the function returns true.
+    // 	   otherwise it will return false. List is unchanged.
 
-  void ResetIterator();
-  // Function: Initializes current position for an iteration through the list.
-  // Pre:  List has been initialized.
-  // Post: Current position is prior to list.
+    void PutItem(T item);
+    // Function: Adds item to list.
+    // Pre:  List has been initialized.
+    //       List is not full.
+    // Post: item is in list.
 
-  int GetNextItem();
-  // Function: Gets the next element in list.
-  // Pre:  List has been initialized and has not been changed since last call.
-  //       Current position is defined.
-  //       Element at current position is not last in list.
-  //	     
-  // Post: Current position is updated to next position.
-  //       item is a copy of element at current position.
+    void DeleteItem(T item);
+    // Function: Deletes the first element whose key matches item's key.
+    // Pre:  ???
+    // Post: ???
+
+    void ResetIterator();
+    // Function: Initializes current position for an iteration through the list.
+    // Pre:  List has been initialized.
+    // Post: Current position is prior to list.
+
+    int GetNextItem();
+    // Function: Gets the next element in list.
+    // Pre:  List has been initialized and has not been changed since last call.
+    //       Current position is defined.
+    //       Element at current position is not last in list.
+    //
+    // Post: Current position is updated to next position.
+    //       item is a copy of element at current position.
 
 private:
-  int length;
-  int info[MAX_ITEMS];
-  int currentPos;
+    int length;
+    Node<T>* head;
+    Node<T>* currentPos;
 };
+
+#include "UnsortedList.cpp"
 
 #endif
